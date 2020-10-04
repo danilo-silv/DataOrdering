a51cdaa120a963a237d9ca121016f517dcd5352c
Danilo Silva <danilo.ssilva12@hotmail.com>
2020-10-04 20:26:42 -0300

    first commit project

---
+++ src/dataordering/DataOrdering.java
@@ -0,0 +1,14 @@
+package dataordering;
+
+/**
+ *
+ * @author danilo
+ * this project is to measure the performance of the ordering of vectors and lists
+ */
+public class DataOrdering {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
